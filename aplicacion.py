5737ccbb7930db0aa628eacc372efbf8b250d95f
Carlos Daniel <1913110305@untels.edu.pe>
2022-11-02 11:10:16 -0500

    Parte 2

---
+++ aplicacion.py
@@ -0,0 +1,34 @@
+import os
+#Crear archivo
+import sys
+def leer_archivo(archivo):
+    archivo= open(archivo,"rt",encoding="utf8")
+    leer_archivo = archivo.read()
+    leer_archivo=leer_archivo.split("\n")
+    return leer_archivo
+ 
+    
+ar_usuario = leer_archivo("login.txt")
+ar_clave = leer_archivo("clave.txt")
+
+
+cont =0
+
+while cont != 2:
+
+    usuario = input("Ingresar Usuario:    ")
+    clave = input("Ingresar Clave:      ")
+    
+    for usuarioItem in ar_usuario:
+        if usuario == usuarioItem:
+            for clave_Item in ar_clave:
+                if clave == clave_Item :
+                    print("*********Datos Ingresados Correctamente**********\n")
+                    print("Datos Personas\n1.Listar Persona\n2.Agregar Personas\n3.Salir")
+                    cont = 2
+                else :
+                    print("**********Datos Incorrectos**********\n")
+                    cont += 1            
+        else :
+            print("**********Datos Incorrectos**********\n")
+            cont += 1
